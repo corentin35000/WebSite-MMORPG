c2345d8ba0b54b79924ae1d0ec994d9d1daa2b65
Coco <corentin.recanzone@epitech.eu>
2021-11-21 18:39:48 +0100

    Structure and install React

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './views/Home';
+import Test from './views/Test';
+import Header from './components/baliseSemantique/Header';
+import Nav from './components/baliseSemantique/Nav';
+import Footer from './components/baliseSemantique/Footer';
 
 export default function App() {
 
   return (
-    <div>
-    </div>
+    <Router>
+        <Header />
+
+        <Nav />
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/test" element={<Test />} />
+        </Routes>
+
+        <Footer />
+    </Router>
   );
 
 }
